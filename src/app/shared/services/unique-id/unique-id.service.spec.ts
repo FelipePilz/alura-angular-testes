d2b146e1f0e8814f29f088094663c3a5aaaa56a2
FelipePilz <felipevilvertpilz@gmail.com>
2021-12-07 19:06:32 -0300

    Colocando contexto na expectativa

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -34,9 +34,11 @@ describe(UniqueIdService.name, () => {
   it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
   should throw
   when called with empty`, () => {
-    const emptyValues = [null, undefined, '', '0'];
+    const emptyValues = [null, undefined, '', '0', 'app'];
     emptyValues.forEach((emptyValue) => {
-      expect(() => service.generateUniqueIdWithPrefix(emptyValue)).toThrow();
+      expect(() => service.generateUniqueIdWithPrefix(emptyValue))
+        .withContext(`Empty Value: ${emptyValue}`)
+        .toThrow();
     });
   });
 });
