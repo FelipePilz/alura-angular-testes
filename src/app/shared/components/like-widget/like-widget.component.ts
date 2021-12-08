793500891d8a578dd25aabd7f1565ede5f5560d8
FelipePilz <felipevilvertpilz@gmail.com>
2021-12-08 17:03:00 -0300

    Implementando o font awesome com o template

--- src/app/shared/components/like-widget/like-widget.component.ts
+++ src/app/shared/components/like-widget/like-widget.component.ts
@@ -1,15 +1,31 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { UniqueIdService } from './../../services/unique-id/unique-id.service';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-like-widget',
   templateUrl: './like-widget.component.html',
   styleUrls: ['./like-widget.component.scss'],
 })
-export class LikeWidgetComponent {
+export class LikeWidgetComponent implements OnInit {
   //Toda vez que der um like vai emitir esse evento
   @Output() public liked = new EventEmitter<void>();
 
   //Total de likes
   @Input() public likes = 0;
   @Input() public id = null;
+
+  public fonts = { faThumbsUp };
+
+  constructor(private uniqueIdService: UniqueIdService) {}
+
+  ngOnInit(): void {
+    if (!this.id) {
+      this.id = this.uniqueIdService.generateUniqueIdWithPrefix('like-widget');
+    }
+  }
+
+  public like(): void {
+    this.liked.emit();
+  }
 }
