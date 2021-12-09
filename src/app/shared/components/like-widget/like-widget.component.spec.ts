bab5d5995c4300f3633331512b14cf98b75c0645
FelipePilz <felipevilvertpilz@gmail.com>
2021-12-09 14:13:50 -0300

    Primeira condição de teste do componente adicionada

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,14 +1,23 @@
-import { TestBed } from '@angular/core/testing';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { UniqueIdService } from './../../services/unique-id/unique-id.service';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LikeWidgetComponent } from './like-widget.component';
 
 describe(LikeWidgetComponent.name, () => {
-  let component: LikeWidgetComponent = null;
+  let fixture: ComponentFixture<LikeWidgetComponent> = null;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LikeWidgetComponent],
+      providers: [UniqueIdService],
+      imports: [FontAwesomeModule],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(LikeWidgetComponent);
   });
 
-  it('', () => {});
+  it('Should create component', () => {
+    const instance = fixture.componentInstance;
+    expect(instance).toBeTruthy();
+  });
 });
