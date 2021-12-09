111eac6d96c4432b9dbf7799e0f83b2e97cda75b
FelipePilz <felipevilvertpilz@gmail.com>
2021-12-09 14:23:41 -0300

    Trocado a abordagem de teste no TestBed

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,5 +1,4 @@
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { UniqueIdService } from './../../services/unique-id/unique-id.service';
+import { LikeWidgetModule } from './like-widget.module';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LikeWidgetComponent } from './like-widget.component';
 
@@ -8,9 +7,7 @@ describe(LikeWidgetComponent.name, () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LikeWidgetComponent],
-      providers: [UniqueIdService],
-      imports: [FontAwesomeModule],
+      imports: [LikeWidgetModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LikeWidgetComponent);
