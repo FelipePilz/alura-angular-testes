073f43ed972bf87e9454a5a5ffdf2d451892f964
FelipePilz <felipevilvertpilz@gmail.com>
2021-12-09 15:57:58 -0300

    Testando propriedade de output de like

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -30,4 +30,14 @@ describe(LikeWidgetComponent.name, () => {
     fixture.detectChanges();
     expect(component.id).toBe(someId);
   });
+
+  it(`#${LikeWidgetComponent.prototype.like.name}
+  should trigger emission
+  when called`, () => {
+    fixture.detectChanges();
+    component.liked.subscribe(() => {
+      expect(true).toBeTrue();
+    });
+    component.like();
+  });
 });
