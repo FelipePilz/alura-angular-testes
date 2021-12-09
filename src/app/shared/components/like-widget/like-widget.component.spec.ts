5e242e3a8b9403a618e21d63eb3ecbdaafed17ee
FelipePilz <felipevilvertpilz@gmail.com>
2021-12-09 16:13:09 -0300

    Simplificando o teste com Spy

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -33,12 +33,10 @@ describe(LikeWidgetComponent.name, () => {
 
   it(`#${LikeWidgetComponent.prototype.like.name}
   should trigger emission
-  when called`, (done) => {
+  when called`, () => {
+    spyOn(component.liked, 'emit');
     fixture.detectChanges();
-    component.liked.subscribe(() => {
-      expect(true).toBeTrue();
-      done();
-    });
     component.like();
+    expect(component.liked.emit).toHaveBeenCalled();
   });
 });
